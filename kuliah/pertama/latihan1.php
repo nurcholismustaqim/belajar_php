84f0d991f35f1005945eb493a03662cc42ed8d76
Nurcholis Mustaqim <nurcholismustaqim15@gmail.com>
2023-09-13 20:57:44 +0700

    Add Folder

---
+++ kuliah/pertama/latihan1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Mahasiswa</title>
+</head>
+
+<body>
+
+  <h3>Daftar Mahasiswa</h3>
+
+  <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>#</th>
+      <th>Gambar</th>
+      <th>NRP</th>
+      <th>Nama</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+      <th>Aksi</th>
+    </tr>
+
+    <tr>
+      <td>1</td>
+      <td><img src="" alt=""></td>
+    </tr>
+  </table>
+
+</body>
+
+</html>
